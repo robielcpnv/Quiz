2f40311943d655552792b27fc94e3cd7296b52f7
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-08 17:00:49 +0100

    Add quetion form

--- lib/pages/quize_page.dart
+++ lib/pages/quize_page.dart
@@ -11,6 +11,42 @@ class QuizePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
+      body: Center(
+          child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child:  const Text(
+              "Choisissez l'adverbe qui est correctement orthographié.",
+              ),
+            ),
+            Container(
+                   margin: EdgeInsets.only(top: 50, bottom:50),
+                  padding: EdgeInsets.all(20),
+                  child:  const Text(
+              "Choisissez l'adverbe qui est correctement orthographié.",
+                  ),
+                ),
+            Container(
+                margin: EdgeInsets.all(5),
+                child: FilledButton(
+                  onPressed: () {},
+                  child: Text('précédament'),
+                )),
+            Container(
+                margin: EdgeInsets.all(5),
+                child: FilledButton(
+                  onPressed: () {},
+                  child: Text('précédemment'),
+                )),
+             Container(
+                margin: EdgeInsets.all(5),
+                child: FilledButton(
+                  onPressed: () {},
+                  child: Text('précédamment'),
+                )),
+          ])),
+
     );
   }
 }
+
